a2d024fb297a0c092f3c7799fb21c46501d38456
Sari Hammad <sari_hammad@hotmail.com>
2025-08-05 19:40:42 -0400

    feat: add Spring Boot main application class

---
+++ backend/src/main/java/ca/devign/orderstream/OrderstreamApplication.java
@@ -0,0 +1,13 @@
+package ca.devign.orderstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderstreamApplication.class, args);
+	}
+
+}
