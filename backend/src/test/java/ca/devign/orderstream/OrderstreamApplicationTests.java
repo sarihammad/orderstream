ecdd80c9296efb7cee3ebba19c4a69f9f30e88b5
Sari Hammad <sari_hammad@hotmail.com>
2025-08-05 19:41:10 -0400

    test: add test configuration and basic application tests

---
+++ backend/src/test/java/ca/devign/orderstream/OrderstreamApplicationTests.java
@@ -0,0 +1,16 @@
+package ca.devign.orderstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class OrderstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test verifies that the Spring application context loads successfully
+	}
+
+}
